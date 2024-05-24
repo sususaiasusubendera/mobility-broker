0b8ec46842b3eecf24f27f260a06c978778b2d80
sususaiasusubendera <oplessnewbee@gmail.com>
2024-05-24 16:36:07 +0700

    seperate server.js into app.js and server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,5 @@
-const express = require("express");
-require("dotenv").config();
+const app = require("./app");
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
