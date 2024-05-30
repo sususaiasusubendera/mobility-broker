91406defd444d580091fbfa61a797d2b9e90abbb
sususaiasusubendera <oplessnewbee@gmail.com>
2024-05-30 07:27:40 +0700

    add auth middleware

---
+++ src/api/middlewares/auth.js
@@ -0,0 +1,24 @@
+const { verifyToken } = require("../utils/jwtUtil");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Access denied!",
+    });
+  }
+
+  try {
+    const user = verifyToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(403).json({
+      message: "invalid token!",
+    });
+  }
+};
+
+module.exports = { authenticateToken };
