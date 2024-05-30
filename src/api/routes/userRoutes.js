3b3e2139b6a51c0ab3336be9ab044d63b2eb7ef3
sususaiasusubendera <oplessnewbee@gmail.com>
2024-05-30 07:33:10 +0700

    add userRoutes

---
+++ src/api/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+const { authenticateToken } = require("../middlewares/auth");
+
+const router = express.Router;
+
+router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+
+// auth protected route
+// router.get('/profile', authenticateToken, (req, res) => {
+//     res.send('This is a protected route.');
+// });
+
+module.exports = router;
