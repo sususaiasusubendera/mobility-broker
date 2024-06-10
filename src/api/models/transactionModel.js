c49cdfaa28181e7a588cc1205041046f6e4a9c0d
sususaiasusubendera <oplessnewbee@gmail.com>
2024-06-11 01:15:08 +0700

    add transactionModel

---
+++ src/api/models/transactionModel.js
@@ -0,0 +1,12 @@
+const pool = require("../configs/databaseConfig");
+
+const getTransactionById = async (id) => {
+    const query = "SELECT * FROM transactions WHERE id = $1";
+    const values = [id];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  };
+
+module.exports = {
+    getTransactionById,
+}
