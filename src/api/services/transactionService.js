c778e8b24747dd639e050e9caa3d5b3ee0c75707
sususaiasusubendera <oplessnewbee@gmail.com>
2024-06-17 17:19:31 +0700

    minor edit transactionService

--- src/api/services/transactionService.js
+++ src/api/services/transactionService.js
@@ -4,8 +4,8 @@ const CustomError = require("../utils/customError");
 const planTrip = async (fromPlace, toPlace, time, date) => {
   try {
     const tripSummaryData = await otpService.planTrip(
-      `${start.lat},${start.lon}`,
-      `${end.lat},${end.lon}`,
+      `${fromPlace.lat},${fromPlace.lon}`,
+      `${toPlace.lat},${toPlace.lon}`,
       time,
       date
     );
