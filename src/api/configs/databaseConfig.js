b04382124f9791c575c749296316d4b4c9b0bcc4
sususaiasusubendera <oplessnewbee@gmail.com>
2024-07-03 11:00:26 +0700

    changed checking environment using process.env in databaseConfig

--- src/api/configs/databaseConfig.js
+++ src/api/configs/databaseConfig.js
@@ -1,29 +1,17 @@
 const { Pool } = require("pg");
 const accessSecret = require("../utils/accessSecret");
-const fs = require("fs");
 
 require("dotenv").config();
 
 const getConfig = async () => {
-  let config = {};
-
-  // check if .env exists
-  if (fs.existsSync(".env")) {
-    config = {
-      DATABASE_USER: process.env.DATABASE_USER,
-      DATABASE_HOST: process.env.DATABASE_HOST,
-      DATABASE_NAME: process.env.DATABASE_NAME,
-      DATABASE_PASS: process.env.DATABASE_PASS,
-      DATABASE_PORT: process.env.DATABASE_PORT,
-    };
-  } else {
+  if (process.env.NODE_ENV === "deployment") {
     try {
-      const secretName = "projects/mobility-1234567/secrets/mobility-secret/versions/latest";
-      config = await accessSecret(secretName);
+      const secretName =
+        "projects/mobility-1234567/secrets/mobility-secret/versions/latest";
+      return await accessSecret(secretName);
     } catch (err) {
-      console.log("Error accessing Secret Manager:", err);
-      console.log("Falling back to environment variables (if any).");
-      config = {
+      console.error("Error accessing Secret Manager:", err);
+      return {
         DATABASE_USER: process.env.DATABASE_USER,
         DATABASE_HOST: process.env.DATABASE_HOST,
         DATABASE_NAME: process.env.DATABASE_NAME,
@@ -31,34 +19,34 @@ const getConfig = async () => {
         DATABASE_PORT: process.env.DATABASE_PORT,
       };
     }
+  } else {
+    return {
+      DATABASE_USER: process.env.DATABASE_USER,
+      DATABASE_HOST: process.env.DATABASE_HOST,
+      DATABASE_NAME: process.env.DATABASE_NAME,
+      DATABASE_PASS: process.env.DATABASE_PASS,
+      DATABASE_PORT: process.env.DATABASE_PORT,
+    };
   }
-
-  return config;
 };
 
 let pool;
 
 // get configuration from secret manager or .env for database connection
 const initializePool = async () => {
-  const config = await getConfig();
+  if (!pool) {
+    const config = await getConfig();
 
-  pool = new Pool({
-    user: config.DATABASE_USER,
-    host: config.DATABASE_HOST,
-    database: config.DATABASE_NAME,
-    password: config.DATABASE_PASS,
-    port: config.DATABASE_PORT,
-  });
+    pool = new Pool({
+      user: config.DATABASE_USER,
+      host: config.DATABASE_HOST,
+      database: config.DATABASE_NAME,
+      password: config.DATABASE_PASS,
+      port: config.DATABASE_PORT,
+    });
+  }
 
   return pool;
 };
-
-// const pool = new Pool({
-//   user: process.env.DATABASE_USER,
-//   host: process.env.DATABASE_HOST,
-//   database: process.env.DATABASE_NAME,
-//   password: process.env.DATABASE_PASS,
-//   port: process.env.DATABASE_PORT,
-// });
 
 module.exports = initializePool;
