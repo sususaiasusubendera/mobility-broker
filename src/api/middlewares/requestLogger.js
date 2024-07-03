3830c4451d8a5dcfba550e3e7cb21870b5c25a48
sususaiasusubendera <oplessnewbee@gmail.com>
2024-07-04 00:21:38 +0700

    edit requestLogger, add NODE_ENV conditional

--- src/api/middlewares/requestLogger.js
+++ src/api/middlewares/requestLogger.js
@@ -2,13 +2,19 @@ const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 
-// create a stream to record the log to a file
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "../../../logs" , "access.log"),
-  { flags: "a" }
-);
+let requestLogger = (req, res, next) => {
+  next();
+};
 
-// morgan configuration to record the log to the console and a file
-const requestLogger = morgan("combined", { stream: accessLogStream });
+if (process.env.NODE_ENV !== "deployment") {
+  // create a stream to record the log to a file
+  const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "../../../logs", "access.log"),
+    { flags: "a" }
+  );
+
+  // morgan configuration to record the log to the console and a file
+  requestLogger = morgan("combined", { stream: accessLogStream });
+}
 
 module.exports = requestLogger;
