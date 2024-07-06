84df3590855f92b5407f4571bfc148a1fcc83d08
sususaiasusubendera <oplessnewbee@gmail.com>
2024-07-06 20:51:32 +0700

    edit response on userController

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -22,7 +22,7 @@ const registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
     const newUser = await userService.createUser({ name, email, password });
-    res.status(201).json(newUser);
+    res.status(201).json({ message: "Account created"});
   } catch (error) {
     next(error);
   }
@@ -33,7 +33,7 @@ const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const { user, token } = await userService.loginUser(email, password);
-    res.status(200).json({ user, token });
+    res.status(200).json({ email: user.email, token });
   } catch (error) {
     next(error);
   }
