d2b10eb6bec3abe17010ff368d84c0df45333f86
sususaiasusubendera <oplessnewbee@gmail.com>
2024-07-07 21:44:58 +0700

    add createTransaction on transactionModel

--- src/api/models/transactionModel.js
+++ src/api/models/transactionModel.js
@@ -1,5 +1,12 @@
 const pool = require("../configs/databaseConfig");
 
+const createTransaction = async (data) => {
+  const { user_id, trip_id, amount, transaction_date } = data; // no qr_link
+  const query = "INSERT INTO transactions (user_id, trip_id, amount, transaction_date) VALUES ($1, $2, $3, $4) RETURNING *";
+  const values = [user_id, trip_id, amount, transaction_date];
+  const result = await pool.query(query, values);
+};
+
 const getTransactionById = async (id) => {
   const query = "SELECT * FROM transactions WHERE id = $1";
   const values = [id];
@@ -8,5 +15,6 @@ const getTransactionById = async (id) => {
 };
 
 module.exports = {
+  createTransaction,
   getTransactionById,
 };
