8612b63cefce127e1b67de57f427c88457ef6447
sususaiasusubendera <oplessnewbee@gmail.com>
2024-07-31 21:25:17 +0700

    delete test

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const now = new Date();
-const expirationDate = new Date(now);
-expirationDate.setDate(now.getDate() + 7);
-expirationDate.setHours(0, 0, 0, 0); 
-
-const expiresIn = Math.floor((expirationDate - now) / 1000);
-
-console.log(expiresIn);
