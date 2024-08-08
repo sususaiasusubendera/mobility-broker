182f386aa0d15cf020412dfc47fecca5f89a2e57
sususaiasusubendera <oplessnewbee@gmail.com>
2024-08-08 21:30:07 +0700

    minor edit userController

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -89,24 +89,6 @@ const checkSession = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-  // const authHeader = req.headers["authorization"];
-  // const token = authHeader && authHeader.split(" ")[1];
-
-  // if (!token) {
-  //   return res.status(401).json({
-  //     message: "Access denied!",
-  //   });
-  // }
-
-  // try {
-  //   const user = await verifyToken(token);
-  //   req.user = user;
-  //   next();
-  // } catch (error) {
-  //   res.status(403).json({
-  //     message: "invalid token!",
-  //   });
-  // }
 };
 
 module.exports = {
